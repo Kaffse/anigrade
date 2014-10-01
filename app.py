8c235aeff66e8ea3fa66a878c61906aa1638ef45
Kaffse <kaffse@gmail.com>
2014-10-01 20:36:55 +0100

    Added bare bones flask setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+@app.route('/')
+def index():
+	return render_template("index.jade")
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
