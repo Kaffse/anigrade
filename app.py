1ecae99b97d400f5140ef511a6fbc446cd7f62e4
Keir Smith <kaffse@gmail.com>
2014-10-28 11:52:10 +0000

    Added index text, added first page template

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 def index():
 	return render_template("index.jade")
 
+@app.route('/dragndrop')
+def dragndrop():
+	return render_template("dragndrop.jade")
+
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
